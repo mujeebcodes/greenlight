deadf17e1b5e8059a788c60e728971f4ba2c3bcf
mujeebcodes <mailmujeeb@icloud.com>
2024-12-09 21:13:21 +0000

    created helper functions and middlewares to send json responses, errors and panic recovery

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -8,6 +8,8 @@ import (
 	"strconv"
 )
 
+type envelope map[string]any
+
 // Retrieve the "id" URL parameter from the current request context, then convert it to
 // an integer and return it. If the operation isn't successful, return 0 and an error.
 func (app *application) readIDParam(r *http.Request) (int64, error) {
@@ -21,8 +23,8 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	return id, nil
 }
 
-func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
-	js, err := json.Marshal(data)
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
+	js, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
 		return err
 	}
