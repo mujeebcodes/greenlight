26b21a36458a094e613eeda0c436dd1b755fcf4e
mujeebcodes <mailmujeeb@icloud.com>
2024-12-20 22:19:16 +0000

    created middleware for authenticating user using stateful authentication tokens

--- internal/data/users.go
+++ internal/data/users.go
@@ -25,6 +25,13 @@ type User struct {
 	Version   int       `json:"-"`
 }
 
+var AnonymousUser = &User{}
+
+// Check if a User instance is the AnonymousUser.
+func (u *User) IsAnonymous() bool {
+	return u == AnonymousUser
+}
+
 // Create a custom password type which is a struct containing the plaintext and hashed
 // versions of the password for a user. The plaintext field is a *pointer* to a string,
 // so that we're able to distinguish between a plaintext password not being present in
