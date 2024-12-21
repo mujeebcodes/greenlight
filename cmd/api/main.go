ad46a8b3fc8ca242c1a5907bfc0fd7ab493c81e3
mujeebcodes <mailmujeeb@icloud.com>
2024-12-21 15:32:12 +0000

    created a middleware to handle cors

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/mujeebcodes/greenlight/internal/mailer"
 	"log/slog"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -36,6 +37,9 @@ type config struct {
 		username string
 		password string
 		sender   string
+	}
+	cors struct {
+		trustedOrigins []string
 	}
 }
 
@@ -69,6 +73,11 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "ef3c89bf1767f2", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "271faa657718e6", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.diplomatmujeeb.net>", "SMTP sender")
+
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
 
 	flag.Parse()
 
